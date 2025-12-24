ddcb01846f6c85f62d5eec3951e46f67a4254b34
Guyass Rozybakiev <rozzybakiev@gmail.com>
2025-12-24 16:03:36 -0500

    Added pomodoro counter and reset with button

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@ int lastButtonState = 0;
 float pomodoro_counter = 0.0;
 String pomodoro_counter_string;
 
+const unsigned long holdtime = 2000; // 2 sec
+
+unsigned long pressStartTime = 0;
+
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
@@ -24,24 +28,43 @@ void setup() {
 	tft.setTextColor(TFT_WHITE, TFT_BLACK);
 	tft.setTextSize(1);
 	tft.setCursor(0, 0);
-	tft.println("Hello World");
+	tft.println("Pomodoro timer");
+
+	tft.setTextColor(TFT_BLUE, TFT_BLACK);
+	tft.setTextDatum(MC_DATUM); // Middle Center
+	tft.drawString("0.00", 65, 75, 1); // Found center
 }
 
 void loop() {
-	// read the state of the pushbutton value:
   	buttonState = digitalRead(buttonPin);
+
+	if (buttonState == HIGH && lastButtonState == LOW)
+	{
+		// Start hold timer
+		pressStartTime = millis();
+	}
 
 	if (buttonState == LOW && lastButtonState == HIGH)
 	{
-		pomodoro_counter += 5.0;
-		pomodoro_counter_string = String(pomodoro_counter);
+		unsigned long pressDuration = millis() - pressStartTime;
 
+		if (pressDuration <= holdtime)
+		{
+			pomodoro_counter += 5.0;
+			Serial.println(pomodoro_counter);
+		} else if (pressDuration >= holdtime) {
+			pomodoro_counter = 0.0;
+			Serial.println("Counter restarted");
+		}
+
+		pomodoro_counter_string = String(pomodoro_counter);
+		tft.fillRect(0, 40, 160, 40, TFT_BLACK); // Clear area to not leave pixels
 		tft.setTextColor(TFT_BLUE, TFT_BLACK);
-		tft.setTextDatum(MC_DATUM);   // Middle Center
+		tft.setTextDatum(MC_DATUM); // Middle Center
 		tft.drawString(pomodoro_counter_string, 65, 75, 1); // Found center
-		Serial.println(pomodoro_counter);
+		
 		delay(200); // 500 ms delay between each increments
 	}
-
+	
 	lastButtonState = buttonState;
 }
