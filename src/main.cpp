ccd12c86625a2a6cf2e30ddee5251589adbbbdef
Guyass Rozybakiev <rozzybakiev@gmail.com>
2025-12-24 15:38:02 -0500

    pomodoro counter setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,47 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <SPI.h>
+
+const int buttonPin = 25;
+int buttonState = 0;
+int lastButtonState = 0;
+float pomodoro_counter = 0.0;
+String pomodoro_counter_string;
 
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
-  Serial.begin(115200);
-  delay(100);
+	Serial.begin(115200);
+	delay(100);
 
-  tft.init();
-  tft.setRotation(1);   // 0–3 if orientation is wrong
-  tft.fillScreen(TFT_BLACK);
+	// initialize the pushbutton pin as an input:
+	pinMode(buttonPin, INPUT);
 
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(2);
-  tft.setCursor(10, 20);
-  tft.println("Hello World");
+	tft.init();
+	tft.setRotation(0);   // 0–3 if orientation is wrong
+	tft.fillScreen(TFT_BLACK);
 
-  tft.setTextSize(1);
-  tft.setCursor(10, 60);
-  tft.println("ESP32 + ST7735");
+	tft.setTextColor(TFT_WHITE, TFT_BLACK);
+	tft.setTextSize(1);
+	tft.setCursor(0, 0);
+	tft.println("Hello World");
 }
 
 void loop() {
-  // nothing
+	// read the state of the pushbutton value:
+  	buttonState = digitalRead(buttonPin);
+
+	if (buttonState == LOW && lastButtonState == HIGH)
+	{
+		pomodoro_counter += 5.0;
+		pomodoro_counter_string = String(pomodoro_counter);
+
+		tft.setTextColor(TFT_BLUE, TFT_BLACK);
+		tft.setTextDatum(MC_DATUM);   // Middle Center
+		tft.drawString(pomodoro_counter_string, 65, 75, 1); // Found center
+		Serial.println(pomodoro_counter);
+		delay(200); // 500 ms delay between each increments
+	}
+
+	lastButtonState = buttonState;
 }
